fb338c3a737275f5fbdae31bcf297c634a7ff60b
sunxiaoguang <438767738@qq.com>
2020-10-28 21:43:51 +0800

    1207.独一无二的出现次数 剑指 Offer 10- II.青蛙跳台阶问题 70.爬楼梯 1289.下降路径最小和  II

---
+++ src/com/sxg/leetcode/question/no0051_0100/Question0070.java
@@ -0,0 +1,48 @@
+package com.sxg.leetcode.question.no0051_0100;
+
+/**
+ * @ClassName: Question0070
+ * @Author: Su_N
+ * @Date: 2020/10/28 20:44
+ * @Description: 动态规划，菲波那切数列
+ * 70. 爬楼梯
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * 注意：给定 n 是一个正整数。
+ *
+ * 示例 1：
+ *
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ *
+ * 示例 2：
+ *
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ */
+public class Question0070 {
+    public int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int index = 2; index <= n; index++) {
+            dp[index] = (dp[index - 1] + dp[index - 2]);
+        }
+
+        return dp[n];
+    }
+}
