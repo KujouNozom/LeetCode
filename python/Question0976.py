c801f2284f0d5f86950bd0ff31fe3f4327e1f2df
Kujou Nozom <438767738@qq.com>
2020-11-29 18:28:37 +0800

    976. 三角形的最大周长

---
+++ python/Question0976.py
@@ -0,0 +1,41 @@
+# 976. 三角形的最大周长
+# [简单]
+# 排序，逆序遍历，两边之和大于第三边
+# 给定由一些正数（代表长度）组成的数组 A，返回由其中三个长度组成的、面积不为零的三角形的最大周长。
+# 如果不能形成任何面积不为零的三角形，返回 0。
+# 示例 1：
+# 输入：[2,1,2]
+# 输出：5
+#
+# 示例 2：
+# 输入：[1,2,1]
+# 输出：0
+#
+# 示例 3：
+# 输入：[3,2,3,4]
+# 输出：10
+#
+# 示例 4：
+# 输入：[3,6,2,3]
+# 输出：8
+#
+# 提示：
+#
+#     3 <= A.length <= 10000
+#     1 <= A[i] <= 10^6
+
+from typing import List
+
+
+class Solution:
+    def largestPerimeter(self, A: List[int]) -> int:
+        A.sort()
+        for index in range(len(A) - 1, 1, -1):
+            value = A[index - 1] + A[index - 2]
+            if A[index] < value:
+                return A[index] + value
+
+        return 0
+
+
+Solution().largestPerimeter([1, 2, 1])
