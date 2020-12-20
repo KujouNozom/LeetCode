62522f57a729ba5a6d8ec837be2f58635b3e62ae
Kujou Nozom <438767738@qq.com>
2020-12-20 20:40:17 +0800

    Create Question0376.java

---
+++ src/com/sxg/leetcode/bad/Question0376.java
@@ -0,0 +1,32 @@
+package com.sxg.leetcode.bad;
+
+/**
+ * @ClassName: Question0376
+ * @Author: Su_N
+ * @Date: 2020/12/12 22:59
+ * @Description:
+ */
+public class Question0376 {
+    public int wiggleMaxLength(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return n;
+        }
+        int[] up = new int[n];
+        int[] down = new int[n];
+        up[0] = down[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up[i] = Math.max(up[i - 1], down[i - 1] + 1);
+                down[i] = down[i - 1];
+            } else if (nums[i] < nums[i - 1]) {
+                up[i] = up[i - 1];
+                down[i] = Math.max(up[i - 1] + 1, down[i - 1]);
+            } else {
+                up[i] = up[i - 1];
+                down[i] = down[i - 1];
+            }
+        }
+        return Math.max(up[n - 1], down[n - 1]);
+    }
+}
