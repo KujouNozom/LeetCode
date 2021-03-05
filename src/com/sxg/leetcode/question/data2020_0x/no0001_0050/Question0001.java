5ac2303397430def01d7f2781db20eeee3ac3219
Kujou Nozom <438767738@qq.com>
2021-03-05 22:18:38 +0800

    整理

--- src/com/sxg/leetcode/question/data2020_0x/no0001_0050/Question0001.java
+++ src/com/sxg/leetcode/question/data2020_0x/no0001_0050/Question0001.java
@@ -2,27 +2,11 @@ package com.sxg.leetcode.question.data2020_0x.no0001_0050;
 
 import java.util.HashMap;
 
-
 /**
  * @ClassName: Question0001
  * @Author: Su_N
  * @Date: 2020/10/3 9:19
- * @Description: hash
- * 使用 hash，遍历数组每个元素 x，查找 target - x 是否存在
- * 1. 两数之和
- *
- * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个整数，并返回他们的数组下标。
- *
- * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
- *
- *
- *
- * 示例:
- *
- * 给定 nums = [2, 7, 11, 15], target = 9
- *
- * 因为 nums[0] + nums[1] = 2 + 7 = 9
- * 所以返回 [0, 1]
+ * @Description: 1. 两数之和 [hash]
  */
 public class Question0001 {
     public int[] twoSum(int[] nums, int target) {
@@ -33,7 +17,7 @@ public class Question0001 {
             int temp = nums[index];
             int ansValue = target - temp;
             if (indexes.containsKey(ansValue)) {
-                return new int[]{indexes.get(ansValue), index};
+                return new int[] {indexes.get(ansValue), index};
             }
             indexes.put(temp, index);
         }
--- src/com/sxg/leetcode/question/data2020_0x/no0001_0050/Question0002.java
+++ src/com/sxg/leetcode/question/data2020_0x/no0001_0050/Question0002.java
@@ -1,26 +1,14 @@
 package com.sxg.leetcode.question.data2020_0x.no0001_0050;
 
-import com.sxg.leetcode.entity.ListNode;
-
 import java.util.Scanner;
+
+import com.sxg.leetcode.entity.ListNode;
 
 /**
  * @ClassName: Question0080
  * @Author: Su_N
  * @Date: 2020/4/26 22:13
  * @Description: 2. 两数相加
- * <p>
- * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
- * <p>
- * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
- * <p>
- * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
- * <p>
- * 示例：
- * <p>
- * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
- * 输出：7 -> 0 -> 8
- * 原因：342 + 465 = 807
  */
 public class Question0002 {
     public static void main(String[] args) {
@@ -63,8 +51,7 @@ public class Question0002 {
         ListNode reultCurrent = reultHead;
 
         while (l1 != null || l2 != null || carry != 0) {
-            int addResult = (l1 == null ? 0 : l1.val)
-                    + (l2 == null ? 0 : l2.val) + carry;
+            int addResult = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
 
             ListNode tempNode = new ListNode(addResult % 10);
             carry = addResult / 10;
